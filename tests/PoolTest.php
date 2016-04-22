96618fbcc6de3fb2e0aa9c437eebbd75ec561d07
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-04-22 23:33:45 +0200

    Fixed tests

--- tests/PoolTest.php
+++ tests/PoolTest.php
@@ -4,6 +4,7 @@ namespace WyriHaximus\React\Tests\ChildProcess\Pool\PhuninNode;
 
 use Phake;
 use WyriHaximus\PhuninNode\Tests\Plugins\AbstractPluginTest;
+use WyriHaximus\React\ChildProcess\Pool\Info;
 use WyriHaximus\React\ChildProcess\Pool\PhuninNode\Pool;
 use WyriHaximus\React\ChildProcess\Pool\PoolInfoInterface;
 
@@ -13,9 +14,10 @@ class PoolTest extends AbstractPluginTest
     {
         $pool = Phake::mock(PoolInfoInterface::class);
         Phake::when($pool)->info()->thenReturn([
-            'size'          => 1,
-            'queued_calls'  => 2,
-            'idle_workers'  => 3,
+            Info::SIZE  => 1,
+            Info::BUSY  => 4,
+            Info::CALLS => 2,
+            Info::IDLE  => 3,
         ]);
         $this->plugin = new Pool($pool, 'a', 'b', 'c');
 
